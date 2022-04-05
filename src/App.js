6ed707aec9c8f37d1f9286146fef2b01bfdf24e1
vitor lopes <lopesvitor959@gmail.com>
2022-04-05 14:52:50 +0100

    final version

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from 'react'
 import Photo from './Photo';
 import { FaSearch } from 'react-icons/fa'
@@ -6,37 +5,42 @@ import { FaSearch } from 'react-icons/fa'
 // unsplash
 const clientID = `?client_id=WWSKTIxshgo6LcWkF4k4OdhHtND2Hb1DXWio77BWf5I`
 const mainUrl = `https://api.unsplash.com/photos/`
-//! HERE **********
 const searchUrl = `https://api.unsplash.com/search/photos/`
 
 function App() {
 
   const [loading, setLoading] = useState(false)
   const [photos, setPhotos] = useState([])
-  const [page, setPage] = useState(1)
-//! HERE **********
+  //* The problem I have to solve is that I'm getting the searchTerm 
+  //  related items but I'm not wiping out the old ones. The ones that come
+  //  from the initia fetch. So ...
+  //*  1) Chaging the initial value of page to 0 won't change the data retrieved
+  // by the fetch. Exactly the same data as for page=1
+  const [page, setPage] = useState(0)
   const [searchTerm, setSearchTerm] = useState("")
   
   const fetchImages = async () => {
     setLoading(true)
     const urlPage = `&page=${page}`
-//! HERE **********
     let url
     const urlSearch = `&query=${searchTerm}`
     if(searchTerm){
          url = `${searchUrl}${clientID}${urlPage}${urlSearch}`
     } else{
          url = `${mainUrl}${clientID}${urlPage}`
+         console.log(url);
     }
  
     const res = await fetch(url)
     const data = await res.json()
-    console.log(data);
-    console.log(url);
-
-//! HERE **********
+   // console.log(data);
+   
     setPhotos(oldPhotos => {
-      if(searchTerm){
+//*  2)
+      if(searchTerm && page === 1){
+        return data.results
+      }
+      else if(searchTerm){
         return [...oldPhotos, ...data.results]
       }else{
         return [...oldPhotos, ...data]
@@ -61,11 +65,12 @@ function App() {
     }
   }, [])
 
-//! HERE **********
+
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log("hello");
-    fetchImages()
+//* 3)
+    // fetchImages()
+    setPage(1)
   }
 
   return (
@@ -75,13 +80,11 @@ function App() {
         <form className="search-form">
           <input className="form-input" type="text"
                  placeholder='Search'
-//! HERE **********
                  value={searchTerm}
                  onChange={ e => setSearchTerm(e.target.value)}      
           >
           </input>
-          <button className='submit-btn' type='submit'
-                  //! HERE **********
+          <button className='submit-btn' type='submit' 
                   onClick={handleSubmit}
           >
               <FaSearch/>
